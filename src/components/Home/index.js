f0b829ed754b81f672d18eea16a80d54767f27a3
Prasanth M <prasanth20042000@gmail.com>
2024-07-03 02:49:40 +0000

    second commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -18,12 +18,24 @@ class Home extends Component {
               <div className={`home-cont-1 ${darkModeClassName}`}>
                 <Header />
                 <div className="about-me-section">
-                  <Typewriter />
-                  <p className={`about-me-para ${darkMode ? 'textDark' : ''}`}>
-                    A passionate individual who always thrives to work on end to
-                    end products which develop sustainable and scalable social
-                    and technical systems to create impact.
-                  </p>
+                  <div>
+                    <Typewriter />
+                    <p
+                      className={`about-me-para ${darkMode ? 'textDark' : ''}`}
+                    >
+                      A passionate individual who always thrives to work on end
+                      to end products which develop sustainable and scalable
+                      social and technical systems to create impact.
+                    </p>
+                  </div>
+                  <div>
+                    <img
+                      className="profile-pic"
+                      src="https://res.cloudinary.com/dkk6a7svu/image/upload/v1719912556/IMG_20240702_145731_emoka4.jpg"
+                      alt="profile-pic"
+                    />
+                    <div className="profile-dot"></div>
+                  </div>
                 </div>
               </div>
             </div>
