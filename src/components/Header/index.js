5e0e377b845a2b730ba02aec968e6034ee62c0ea
Prasanth M <prasanth20042000@gmail.com>
2024-07-24 07:49:50 +0000

    changes in navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -60,13 +60,16 @@ class Header extends Component {
                 />
                 <div className="nav-ham-cont">
                   <DarkModeSwitch changeToDarkMode={changeDarkMode} />
-                  <button
-                    type="button"
-                    className="ham-btn"
-                    onClick={this.onClickHamBtn}
-                  >
-                    <MdSort />
-                  </button>
+                  <label class="hamburger">
+                    <input type="checkbox" onChange={this.onClickHamBtn} />
+                    <svg viewBox="0 0 32 32">
+                      <path
+                        class="line line-top-bottom"
+                        d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+                      ></path>
+                      <path class="line" d="M7 16 27 16"></path>
+                    </svg>
+                  </label>
                 </div>
               </div>
               <div className={`nav-cont-2 ${navLinksClassName}`}>
